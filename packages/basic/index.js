59cb3f02d5ebdffa523432f90ff7292c4aa283c5
nikkolast88 <nikkolast88@gmail.com>
2023-01-05 10:29:02 +0800

    fix：bem格式，vue

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -4,6 +4,30 @@ module.exports = {
     'stylelint-config-rational-order',
     'stylelint-config-prettier'
   ],
-  plugins: ['stylelint-declaration-block-no-ignored-properties'],
-  rules: {}
+  plugins: [
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-selector-bem-pattern'
+  ],
+  overrides: [
+    {
+      files: '**/*.scss',
+      customSyntax: 'postcss-scss'
+    }
+  ],
+  rules: {
+    'plugin/selector-bem-pattern': {
+      preset: 'bem',
+      utilitySelectors: /^\.[util|u]-[a-z]+$/,
+      ignoreSelectors: '\\b(svg|path)\\b'
+    },
+    'selector-class-pattern': [
+      '^[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$',
+      {
+        // TODO: custom formatters(https://stylelint.io/developer-guide/formatters/)
+        // message(prop, value) {
+        //   return "https://github.com/AndyOGo/stylelint-declaration-strict-value/issues/142";
+        // },
+      }
+    ]
+  }
 }
--- packages/vue/index.js
+++ packages/vue/index.js
@@ -2,7 +2,14 @@ module.exports = {
   extends: [
     '@uni-daily/stylelint-config-basic',
     'stylelint-config-standard-scss',
-    'stylelint-config-recommended-vue/scss'
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-prettier'
+  ],
+  overrides: [
+    {
+      files: '**/*.vue',
+      customSyntax: 'postcss-html'
+    }
   ],
   rules: {}
 }
